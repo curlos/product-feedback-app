ac1fbfd13bfe0802f82d5ff5c06ff8fb98d1301b
Carlos Martinez <curlosmart@gmail.com>
2022-03-01 21:18:13 -0500

    Added tags card

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/LeftSidebar.module.scss'
 
 const LeftSidebar = () => {
+
+  const tags = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature']
+  const [selected, setSelected] = useState('All')
+
   return (
     <div className={styles.container}>
       <div className={styles.cardBoardName}>
@@ -9,7 +13,14 @@ const LeftSidebar = () => {
         <p>Feedback Board</p>
       </div>
 
-      
+      <div className={styles.cardBoardTags}>
+        {tags.map((tag) => (
+          <div key={tag} className={`${styles.tag}${tag === selected ? (' ' + styles.selected) : ''}`}>
+            {tag}
+          </div>
+        ))}
+        
+      </div>
     </div>
   )
 }
